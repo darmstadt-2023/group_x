66936a41892fb70734f8f66209a318d63c610b48
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-04-24 14:15:43 +0300

    database dump made

--- database_dump.sql
+++ database_dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `university` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `university`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: university
@@ -110,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-24 14:12:44
+-- Dump completed on 2023-04-24 14:15:33
